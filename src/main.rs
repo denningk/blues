caa2a9df9636c28f56871f4a7b38f86233c6b34a
Keith Denning <kdenning@captechventures.com>
2020-07-06 10:17:18 -0400

    Create main function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+mod render_engine;
+
+use render_engine::display_manager;
+
 fn main() {
     println!("Hello, world!");
+    let (mut window, events, mut glfw) = display_manager::create_display();
+
+    // render loop
+    while !window.should_close() {
+        display_manager::update_display(&mut window, &events, &mut glfw);
+    }
+
+    display_manager::close_display(window);
 }
