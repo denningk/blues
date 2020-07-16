2213eb7a533d8989a7544c8d5a3e725be3a542cc
Keith Denning <kdenning@captechventures.com>
2020-07-16 12:30:38 -0400

    Update shader

--- src/toolbox/math.rs
+++ src/toolbox/math.rs
@@ -1,6 +1,6 @@
 use cgmath::{Vector3, Matrix4, vec3, Deg};
 
-fn create_transformation_matrix(translation: &Vector3<f32>, rx: f32, ry: f32,
+pub fn create_transformation_matrix(translation: &Vector3<f32>, rx: f32, ry: f32,
     rz: f32, scale: f32) -> Matrix4<f32> {
         let mut matrix = Matrix4::<f32>::from_translation(*translation);
         matrix = matrix * Matrix4::<f32>::from_axis_angle(vec3(1.0, 0.0, 0.0), Deg(rx));
